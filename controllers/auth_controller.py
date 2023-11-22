3742e64854ff8589ad7dbca147cce2e7757d2176
Hugo <101125294+Lony027@users.noreply.github.com>
2023-11-22 21:13:08 +0100

    fix: useless import refactoring

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,15 +1,9 @@
-from flask import Flask, jsonify
+from flask import jsonify
+from flask import request
 from flask import Blueprint
-
-from flask import Flask, request, jsonify
-from flask_restful import reqparse, abort, Api, Resource
-from flask_cors import CORS
-from contextlib import closing
-from config import config
 
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
 
 from services.auth_service import auth_service
