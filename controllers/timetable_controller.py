825d7af377d170c20de2d1d91ddb543bf88e70f0
Hugo <101125294+Lony027@users.noreply.github.com>
2023-11-30 15:12:38 +0100

    feat: by student

--- controllers/timetable_controller.py
+++ controllers/timetable_controller.py
@@ -6,16 +6,15 @@ from services.timetable_service import timetable_service
 
 timetable_app = Blueprint('timetable_app', __name__)
 
-@timetable_app.route('/timetable/get/bygroup', methods=['POST'])
+    
+@timetable_app.route('/timetable/get/byroom', methods=['POST'])
 #@jwt_required()
-def get_timetable_by_group():
+def get_timetable_by_room():
     """ Get timetable by group"""
     data = request.json
     _service = timetable_service()
     returnStatement = _service.get_timetable_by_room(data)
     if returnStatement == 'Invalid argument':
-        return returnStatement
-    elif returnStatement == 'Room does not exist':
         return returnStatement
     else:
         return jsonify({'edt' : returnStatement})
@@ -23,7 +22,7 @@ def get_timetable_by_group():
 @timetable_app.route('/timetable/get/byteacher', methods=['POST'])
 #@jwt_required()   
 def get_timetable_by_teacher():
-    """ Get timetable by personnal_id"""
+    """ Get timetable by teacher_id"""
     data = request.json
     _service = timetable_service()
     returnStatement = _service.get_timetable_by_teacher(data)
@@ -35,7 +34,7 @@ def get_timetable_by_teacher():
 @timetable_app.route('/timetable/get/byprom', methods=['POST'])
 #@jwt_required()   
 def get_timetable_by_prom():
-    """ Get timetable by personnal_id"""
+    """ Get timetable by teacher_id"""
     data = request.json
     _service = timetable_service()
     returnStatement = _service.get_timetable_by_prom(data)
@@ -44,7 +43,18 @@ def get_timetable_by_prom():
     else:
         return jsonify({'edt' : returnStatement})
     
-
+@timetable_app.route('/timetable/get/bystudent', methods=['POST'])
+#@jwt_required()   
+def get_timetable_by_student():
+    """ Get timetable by teacher_id"""
+    data = request.json
+    _service = timetable_service()
+    returnStatement = _service.get_timetable_by_student(data)
+    if returnStatement == 'Invalid argument':
+        return returnStatement
+    else:
+        return jsonify({'edt' : returnStatement})
+    
 #via promo
 
 #via userId (pr le dashboard n'affiche que ses groupes)
--- services/timetable_service.py
+++ services/timetable_service.py
@@ -7,74 +7,47 @@ class timetable_service(Service):
 
     #@todo invalid date exception, + rajouter exception au contrat
     def get_timetable_by_room(self, data):
-        room_name = data.get('room_name', '')
+        room_id = data.get('room_id', '')
         week_date_start = data.get('week_date_start', '')
         week_date_end = data.get('week_date_end', '')
 
-        if room_name == '' or week_date_start == '' or week_date_end == '':
+        if room_id == '' or week_date_start == '' or week_date_end == '':
             return "Null arguments"
-            
-        query = "SELECT id FROM university.rooms WHERE code = '" + str(room_name) + "'"
-        conn = self.get_connection()
-        rows = connect_pg.get_query(conn, query)
-
-        if len(rows) == 0: 
-            return "Room does not exist"
-
-        room_id = rows[0][0]
 
         week_date_start = datetime.datetime.strptime(week_date_start,"%Y-%m-%d")
         week_date_end = datetime.datetime.strptime(week_date_end,"%Y-%m-%d")
 
-        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd HH24:MI:SS'), duree 
+        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd"T"HH24:MI'), duree, rooms.code 
                     FROM university.courses 
                     INNER JOIN university.teachings ON university.courses.teaching_id = university.teachings.id
                     INNER JOIN university.personals ON university.courses.personal_id = university.personals.id
-                    WHERE rooms_id =""" +  str(room_id) + """ AND
+                    INNER JOIN university.rooms ON university.courses.rooms_id = university.rooms.id
+                    WHERE university.rooms.id =""" +  str(room_id) + """ AND
                     starttime >= '""" + str(week_date_start) + """' AND starttime <= '""" + str(week_date_end) + """'"""
-        #utiliser model?
-        rows = connect_pg.get_query(conn, query)
-        returnStatement = []
-        for row in rows:
-            row = row + (room_name,)
-            returnStatement.append(self.get_student_statement(row))
-        connect_pg.disconnect(conn)
-        return returnStatement
+        return self.execute_query_and_get_statement(query)
+
     
     def get_timetable_by_teacher(self, data):
-        # rajouter try catch si teacher n'existe pas
         personnal_id = data.get('personnal_id', '')
         week_date_start = data.get('week_date_start', '')
         week_date_end = data.get('week_date_end', '')
 
         if personnal_id == '' or week_date_start == '' or week_date_end == '':
             return "Null arguments"
-        
 
         week_date_start = datetime.datetime.strptime(week_date_start,"%Y-%m-%d")
         week_date_end = datetime.datetime.strptime(week_date_end,"%Y-%m-%d")
 
-        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd HH24:MI:SS'), duree, rooms.code 
+        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd"T"HH24:MI'), duree, rooms.code 
                     FROM university.courses 
                     INNER JOIN university.teachings ON university.courses.teaching_id = university.teachings.id
                     INNER JOIN university.personals ON university.courses.personal_id = university.personals.id
                     INNER JOIN university.rooms ON university.courses.rooms_id = university.rooms.id
                     WHERE university.personals.id =""" +  str(personnal_id) + """ AND
                     starttime >= '""" + str(week_date_start) + """' AND starttime <= '""" + str(week_date_end) + """'"""
-        print(query)
-
-        #utiliser model?
-        conn = self.get_connection()
-        rows = connect_pg.get_query(conn, query)
-
-        returnStatement = []
-        for row in rows:
-            returnStatement.append(self.get_student_statement(row))
-        connect_pg.disconnect(conn)
-        return returnStatement
+        return self.execute_query_and_get_statement(query)
     
     def get_timetable_by_prom(self, data):
-        # rajouter try catch si teacher n'existe pas
         promotion_id = data.get('promotion_id', '')
         department_id = data.get('department_id', '')
         week_date_start = data.get('week_date_start', '')
@@ -83,11 +56,10 @@ class timetable_service(Service):
         if promotion_id == '' or department_id == '' or week_date_start == '' or week_date_end == '':
             return "Null arguments"
         
-
         week_date_start = datetime.datetime.strptime(week_date_start,"%Y-%m-%d")
         week_date_end = datetime.datetime.strptime(week_date_end,"%Y-%m-%d")
 
-        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd HH24:MI:SS'), duree, rooms.code
+        query = """SELECT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd"T"HH24:MI'), duree, rooms.code
                 FROM university.courses
 
                 INNER JOIN university.personals ON university.courses.personal_id = university.personals.id
@@ -101,9 +73,38 @@ class timetable_service(Service):
                 WHERE university.groups.promotion =""" +  str(promotion_id) + """ AND
                 university.groups.department_id =""" + str(department_id) + """ AND
                 starttime >= '""" + str(week_date_start) + """' AND starttime <= '""" + str(week_date_end) + """'"""
-        print(query)
 
-        #utiliser model?
+        return self.execute_query_and_get_statement(query)
+
+    def get_timetable_by_student(self, data):
+        student_id = data.get('student_id', '')
+        week_date_start = data.get('week_date_start', '')
+        week_date_end = data.get('week_date_end', '')
+
+        if student_id == '' or week_date_start == '' or week_date_end == '':
+            return "Null arguments"
+
+        week_date_start = datetime.datetime.strptime(week_date_start,"%Y-%m-%d")
+        week_date_end = datetime.datetime.strptime(week_date_end,"%Y-%m-%d")
+
+        query = """SELECT DISTINCT courses.description, course_type, personals.last_name, personals.first_name, teachings.title, TO_CHAR(starttime, 'yyyy-mm-dd"T"HH24:MI'), duree, rooms.code
+                FROM university.courses
+
+                INNER JOIN university.personals ON university.courses.personal_id = university.personals.id
+                INNER JOIN university.teachings ON university.courses.teaching_id = university.teachings.id
+                INNER JOIN university.rooms ON university.courses.rooms_id = university.rooms.id
+
+                INNER JOIN university.participates ON university.courses.id = university.participates.course_id
+                INNER JOIN university.students ON university.participates.subgroup_id = university.students.subgroup_id
+
+                WHERE university.students.student_number = '""" +  str(student_id) + """' AND
+                starttime >= '""" + str(week_date_start) + """' AND starttime <= '""" + str(week_date_end) + """'"""
+        print(query)
+        return self.execute_query_and_get_statement(query)
+
+
+
+    def execute_query_and_get_statement(self, query):
         conn = self.get_connection()
         rows = connect_pg.get_query(conn, query)
 
@@ -119,7 +120,7 @@ class timetable_service(Service):
             'course_type': row[1],   # Type de cours, Contrôle, Tp, Td, Cours normal/amphi
             'personal_last_name': row[2],
             'personal_first_name': row[3],
-            'teachings.title': row[4],      # r1-01 développement web, sae, c'est les ressources
+            'teaching_title': row[4],      # r1-01 développement web, sae, c'est les ressources
             'starttime': row[5],         # Le nom de famille de l'étudiant
             'duree':  str(row[6]),        # Le prénom de l'étudiant
             'room_name':  row[7]
