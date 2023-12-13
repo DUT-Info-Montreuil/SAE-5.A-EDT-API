2ad550f542f21082d24e75c596fb4c68595fca39
Aldric <105204520+Aldriculteur@users.noreply.github.com>
2023-12-13 17:02:19 +0100

    feat: user add

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -14,3 +14,4 @@ from controllers.absent_controller import absent_app
 from controllers.participate_controller import participate_app
 from controllers.auth_controller import auth_app
 from controllers.timetable_controller import timetable_app
+from controllers.user_controller import user_app
---
+++ controllers/user_controller.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+from flask import request
+from flask import Blueprint
+
+from services.user_service import user_service
+
+user_app = Blueprint('user_app', __name__)
+
+#should it be protected?, do we need an endpoint ? 
+@user_app.route('/user/add', methods=['PUT'])
+def add_user():
+    """ Get all students in JSON format """
+    data = request.json
+    _service = user_service()
+    returnStatement = _service.add_user(data)
+    return jsonify(returnStatement)
