2947a94f785e3bb67463dc37fc01ca4abbb3f2c6
Mehedi Toure <101273741+MehediT@users.noreply.github.com>
2023-12-17 16:42:37 +0100

    feat : sample controller use with sql alchemy

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, jsonify, request
 from services.user_service import UserService
+
 user_app = Blueprint('user_app', __name__)
 userService = UserService()
 
-@user_app.route('/users', methods=['GET'])
+@user_app.route('/users/get', methods=['GET'])
 def get_users():
     users = userService.get_all_users()
     return jsonify({'users': users})
 
-@user_app.route('/users/<int:user_id>', methods=['GET'])
+@user_app.route('/users/get/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
     user = userService.get_user_by_id(user_id)
 
@@ -18,7 +19,7 @@ def get_user_by_id(user_id):
         return jsonify({'message': 'User not found'}), 404
 
 
-@user_app.route('/users/find', methods=['POST'])
+@user_app.route('/users/identify', methods=['POST'])
 def find_user():
     data = request.get_json()
     user = userService.find_user(**data)
@@ -28,7 +29,7 @@ def find_user():
     else:
         return jsonify({'message': 'User not found'}), 404
     
-@user_app.route('/users/<int:user_id>', methods=['PUT'])
+@user_app.route('/users/update/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     data = request.get_json()
     success = userService.update_user(user_id, data)
@@ -38,7 +39,7 @@ def update_user(user_id):
     else:
         return jsonify({'message': 'User not found'}), 404
     
-@user_app.route('/users/<int:user_id>', methods=['DELETE'])
+@user_app.route('/users/delete/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     success = userService.delete_user(user_id)
 
@@ -48,9 +49,13 @@ def delete_user(user_id):
         return jsonify({'message': 'User not found'}), 404
 
 #should it be protected?, do we need an endpoint ? 
-@user_app.route('/user/add', methods=['PUT'])
+@user_app.route('/users/add', methods=['PUT'])
 def add_user():
     """ Get all students in JSON format """
     data = request.json
-    returnStatement = userService.add_user(data)
-    return jsonify(returnStatement)
+    success = userService.add_user(data)
+    
+    if success:
+        return jsonify(success)
+    else:
+        return jsonify({'message': 'User not add'}), 404
